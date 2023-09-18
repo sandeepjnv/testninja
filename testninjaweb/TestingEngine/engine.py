2ba5ae45a31d65ae3ef1ff62f4101dd06720c932
sandeepjnv <sandeepsatheesanjnv@gmail.com>
2023-09-19 01:40:36 +0530

    Added - New interaction ,Updated gitignore

--- testninjaweb/TestingEngine/engine.py
+++ testninjaweb/TestingEngine/engine.py
@@ -2,6 +2,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
 import time
 
@@ -28,30 +29,46 @@ def executeWorkflow(workflow):
             value = component["value"]
 
             # element
-            if component["identifier"] == "id":
-                elementStr = (By.ID,value)
-                element = driver.find_element(By.ID,value)
-            elif component["identifier"] == "xpath":
-                elementStr = (By.XPATH,value)
-                element = driver.find_element(By.XPATH,value)
-            elif component["identifier"] == "select":
-                elementStr = (Select(driver.find_element(By.ID,value)))
-                element = Select(driver.find_element(By.ID,value))
+            if(action["actionType"] not in ["wait",]):
+                if component["identifier"] == "id":
+                    elementStr = (By.ID,value)
+                    element = driver.find_element(By.ID,value)
+                elif component["identifier"] == "xpath":
+                    elementStr = (By.XPATH,value)
+                    element = driver.find_element(By.XPATH,value)
+                elif component["identifier"] == "select":
+                    elementStr = (Select(driver.find_element(By.ID,value)))
+                    element = Select(driver.find_element(By.ID,value))
             
 
             # interaction 
             try:
                 if(action["actionType"] == "write"):
+                    element.send_keys("")
                     element.send_keys(action["value"])
-                if(action["actionType"] == "click"):
+                    element.send_keys(Keys.TAB)
+                elif(action["actionType"] == "forcewrite"):
+                    driver.execute_script("arguments[0].value = arguments[1];", element,action["value"])
+                    driver.execute_script("arguments[0].dispatchEvent(new Event('change', { bubbles: true }));", element)
+                    element.send_keys("")
+                    element.send_keys(Keys.TAB)
+                elif(action["actionType"] == "click"):
                     element.click()
-                if(action["actionType"] == "read"):
-                    data = element.text
-                if(action["actionType"] == "drop"):
+                elif(action["actionType"] == "drop"):
                     element.select_by_value(action["value"])
-                if(action["actionType"] == "element-wait"):
+                elif(action["actionType"] == "element-wait"):
                     wait = WebDriverWait(driver, 20)
                     wait.until(EC.visibility_of_element_located(elementStr))
+                elif(action["actionType"]=="wait"):
+                    time.sleep(int(action["value"]))
+                elif(action["actionType"] == "scrolltoview"):
+                    element.location_once_scrolled_into_view
+
+
+                elif(action["actionType"] == "validate"):
+                    data = element.text
+                    if action['value'] in data:
+                        print("Test Case : Passed")
 
                 # action types
                 # if(action["type"] == "validation"):
